20d85e9577f02a8abe8a775bc70258fa7d829bcb
Juliana S <julianabrunahh@gmail.com>
2022-07-18 15:26:31 -0300

    Initial commit

---
+++ app/clienteapp.java
@@ -0,0 +1,11 @@
+package app;
+import app.conta.cliente;
+
+public class clienteapp{
+    Cliente c1 = new cliente();
+    c1.nome = "Juliana";
+    c1.conta = 9090998;
+    c1.agencia = 4040;
+
+    c1.ExibirDados();
+}
---
+++ conta/cliente.java
@@ -0,0 +1,15 @@
+package conta;
+public class cliente{
+    String nome;
+    int conta;
+    int agencia;
+    public void ExibirDados() {
+       System.out.println("Os dados do cliente: ");
+       System.out.println(nome);
+       System.out.println(agencia);
+       System.out.println(conta);
+
+    }
+
+
+}
